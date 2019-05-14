4a9a03d1d4c7674365c9a6432fbb8fc8bd0d9518
user <user@IN302>
2019-05-14 12:37:29 +0900

    change

--- boot-thymeleaf/src/main/resources/static/js/main.js
+++ boot-thymeleaf/src/main/resources/static/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     $("#regform").submit(function (event) {
-        //stop submit the form, we will post it manually.
         event.preventDefault();
         fire_ajax_submit();
     });
@@ -9,17 +8,16 @@ $(document).ready(function () {
 });
 
 function fire_ajax_submit() {
-   
-   //var data = $("#regform").serializeArray();
-   var data = {
-         "name" : $('#name').val(),
-         "company" : $('#company').val()
-   };
-   console.log(data);
-   var json = JSON.stringify(data);
-   console.log(json);
-   
-   
+
+    //var data = $("#regform").serializeArray();
+    
+	var data = {
+			"name" : $('#name').val(),
+			"company" : $('#company').val()
+	};
+    console.log(data);
+    var json = JSON.stringify(data);
+    console.log(json);
     $.ajax({
         type: "POST",
         contentType: "application/json",
@@ -29,8 +27,8 @@ function fire_ajax_submit() {
         cache: false,
         timeout: 600000,
         success: function (data) {
-           console.log("SUCCESS : ", data);
-           window.location.href = "/users";
+        	console.log("SUCCESS : ", data);
+        	window.location.href = "/users";
         },
         error: function (e) {
             console.log("ERROR : ", e);
